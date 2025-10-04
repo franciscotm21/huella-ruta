329745e626d9e9df9a94fd268c5169a69fdb5129
franciscotm21 <franciscoajtm@gmail.com>
2025-10-04 02:37:44 -0300

    cambio: actualizo portada y factores

--- src/components/HuellaConPortada.tsx
+++ src/components/HuellaConPortada.tsx
@@ -953,7 +953,7 @@ function CenterText({ viewBox, totalKg }: any) {
                 </div>
                 <div style={{ transform: 'scale(0.92)', transformOrigin: 'center' }}>
              <ResponsiveContainer width="100%" height={320}>
-  <PieChart margin={{ top: 10, right: 150, bottom: 10, left: 20 }}>
+  <PieChart margin={{ top: 10, right: 140, bottom: 10, left: 20 }}>
     <defs>
       <filter id="softShadow2" x="-20%" y="-20%" width="140%" height="140%">
         <feDropShadow dx="0" dy="3" stdDeviation="3" floodColor="#000000" floodOpacity="0.12" />
@@ -964,7 +964,7 @@ function CenterText({ viewBox, totalKg }: any) {
       data={donutData}
       dataKey="kg"
       nameKey="name"
-      cx="66%"
+      cx="67%"
       cy="46%"
       innerRadius={76}
       outerRadius={108}
